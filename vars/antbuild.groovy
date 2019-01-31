7028c4f8014e64a98bd797e7b85a2b03edb40ada
rahulmittal901 <32807973+rahulmittal901@users.noreply.github.com>
2019-01-31 13:31:22 +0530

    Update antbuild.groovy
    new change groovy script to run ant build

--- vars/antbuild.groovy
+++ vars/antbuild.groovy
@@ -1,6 +1,8 @@
 #!/usr/bin/env groovy
 
 def call(args) {
- 
-  bat "${tool 'ant'}/bin ${args}"
+ def antVersion = 'ant'
+withEnv( ["ANT_HOME=${tool antVersion}"] ) {
+ bat '%ANT_HOME%/bin/ant.bat ${args}'
+}
 }
